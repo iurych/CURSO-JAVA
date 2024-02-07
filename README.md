bcccf4fdb9a6be939e4dad9f744a5c0df040adfa
iurych <iury.vibe@gmail.com>
2024-02-07 11:42:34 -0300

    chore: change value type due to the withdraw is always be in int type

---
+++ README.md
@@ -0,0 +1,14 @@
+# Projeto para estudo Java
+
+### Aqui é feito um código básico sobre saque em caixa eletrônico. Onde o usuário informa o valor do saque e o sistema retorna a quantidade de notas de 100, 50, 20, 10, 5 e 2 reais.
+
+### Também é verificado casos de sucesso e erro do saque, seguindo regras de negócio.
+
+
+### Obs: Outras funcionalidade serão implementadas ao avançar dos estudos.
+
+### Regras de negócio:
+- O valor do saque deve ser maior que 10 e menor que 1000.
+- O valor do saque pode ser feito nas notas de 10, 20, 50, 100.
+
+
--- src/Withdraw.java
+++ src/Withdraw.java
@@ -1,12 +1,15 @@
 import java.util.Scanner;
 
 public class Withdraw {
-    public static void main(String[] args) {
+    public static <notas> void main(String[] args) {
 
+        int valor;
         String nome;
+
         int VALOR_DE_SAQUE_MAXIMO = 1000;
         int VALOR_DE_SAQUE_MINIMO = 10;
-        double valor;
+
+        int[] notas = {10, 20, 50, 100};
 
         var saldo = Math.random() * 1000;
 
@@ -21,20 +24,31 @@ public class Withdraw {
         String valorFormatado = String.format("%.2f", valor);
         String salddFormatado = String.format("%.2f", saldo);
 
-       if( valor >= VALOR_DE_SAQUE_MINIMO  && valor <= VALOR_DE_SAQUE_MAXIMO ) {
-           if (valor <= saldo) {
-//               saldo -= valor;
-               System.out.println("Olá " + nome + ", o saque de R$" + valorFormatado + " foi realizado com sucesso!");
-               System.out.println("Saldo atual: " + salddFormatado);
-           } else {
-               System.out.println("Saldo insuficiente");
-               System.out.println("Saldo atual: " + salddFormatado);
-           }
-       } else {
-           System.out.println("Olá " + nome + ", o saque de R$" + valorFormatado + " não pode ser realizado devido ao valor indicado ser ou menor que R$10,00 ou maior que R$1000,00.");
+        // preciso verificar se o valor que eu quero sacar é possível de acordo com as notas em dinheiro presentes no caixa eletronico
+        // se a divisão der resto difereten de zero, eu devo pegar apenas o quociente e multiplicar pelo valor da nota
+        // do resto, eu diminuo do produto e faço a divisão novamente pela nota mais baixa que a usada anteriormente
+        public static void verifica_condiçao_saque (notas) {
+           for( int nota : notas ) {
+                if (valor % nota == 0) {
 
-       }
+                }
+           }
+        }
+        if (valor <= saldo) {
+            // saldo -= valor;
+            System.out.println("Olá " + nome + ", o saque de R$" + valorFormatado + " foi realizado com sucesso!");
+            System.out.println("Saldo atual: " + salddFormatado);
+        }
+        else {
+            System.out.println("Saldo insuficiente");
+            System.out.println("Saldo atual: " + salddFormatado);
+        }
+        else{
+            System.out.println("Olá " + nome + ", o saque de R$" + valorFormatado + " não pode ser realizado devido ao valor indicado ser ou menor que R$10,00 ou maior que R$1000,00.");
+
+        }
     }
 }
+
 
 
